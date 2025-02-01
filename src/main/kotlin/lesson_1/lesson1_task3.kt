50a2f38ef6eed308f2c4d7397f9ebbd431dfc470
Teeleste <gnu4y.nikita2014@gmail.com>
2025-02-01 16:11:42 +0300

    KS-1-3 task do

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_1
+
+/* Решение к уроку 1 Задаче 2
+продумать релевантные названия и корректно объявить 2 переменные, которые будут содержать:
+ – количество заказов,
+ – текст с благодарностью за покупку (текст на твое усмотрение).
+ */
+
+fun main() {
+    var numberOfOrders: Int = 69
+    val thxMessage: String = "Спасибо за заказ! Обязательно возвращайтесь еще раз."
+    var NumberOfEmployees: Int = 2000
+
+    println(numberOfOrders)
+    println(thxMessage)
+    //println(NumberOfEmployees)
+
+    NumberOfEmployees = 1999
+
+    println(NumberOfEmployees)
+}

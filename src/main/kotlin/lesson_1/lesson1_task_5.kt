df845c98fe6d33a0bd5151d0597d033ba1ba2a92
Teeleste <gnu4y.nikita2014@gmail.com>
2025-02-02 01:21:41 +0300

    KS-1-5 from seconds to time

---
+++ src/main/kotlin/lesson_1/lesson1_task_5.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_1
+
+fun main(){
+    var secondInSpace: Int = 6480
+    val hourInSpace = secondInSpace / 60 / 60
+    var minuteInSpace = secondInSpace / 60 - (60 * hourInSpace)
+    secondInSpace -= ((60 * hourInSpace + minuteInSpace) * 60)
+
+    print("0" + hourInSpace + ":" + minuteInSpace + ":" + "0" + secondInSpace)
+}

85781876c2d9f3e6fddab9807ed41840f150d87d
Teeleste <gnu4y.nikita2014@gmail.com>
2025-02-04 10:37:07 +0300

    KS-3-4 Stroke details

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+    var whereFrom = "E2"
+    var where = "E4"
+    var moveNumber = 1
+
+    println("$whereFrom-$where;$moveNumber")
+
+    whereFrom = "D2"
+    where = "D3"
+    moveNumber = 2
+
+    println("$whereFrom-$where;$moveNumber")
+
+}

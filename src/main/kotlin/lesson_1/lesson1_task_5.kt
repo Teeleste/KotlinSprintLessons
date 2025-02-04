b03d6555796fbaafbd5e75e4065d24fb47e94735
Teeleste <gnu4y.nikita2014@gmail.com>
2025-02-04 08:14:51 +0300

    KS-1-5 rename const

--- src/main/kotlin/lesson_1/lesson1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson1_task_5.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
-const val constantTime = 60
+const val CONSTANT_TIME = 60
 
 fun main(){
     var secondInSpace: Int = 6480
-    val hourInSpace = secondInSpace / constantTime / constantTime
+    val hourInSpace = secondInSpace / CONSTANT_TIME / CONSTANT_TIME
 
-    val minuteInSpace = secondInSpace / constantTime - (constantTime * hourInSpace)
-    secondInSpace -= ((constantTime * hourInSpace + minuteInSpace) * constantTime)
+    val minuteInSpace = secondInSpace / CONSTANT_TIME - (CONSTANT_TIME * hourInSpace)
+    secondInSpace -= ((CONSTANT_TIME * hourInSpace + minuteInSpace) * CONSTANT_TIME)
 
     val timeFormat = String.format("0%d:%d:%d0", hourInSpace, minuteInSpace, secondInSpace)
 

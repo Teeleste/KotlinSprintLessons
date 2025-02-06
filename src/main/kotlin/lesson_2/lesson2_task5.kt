55f85fb65b25aebb6ed60fa347ce7ecc85560c41
Teeleste <gnu4y.nikita2014@gmail.com>
2025-02-06 17:38:21 +0300

    KS-2-5 replacement math.pow()

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,10 +1,21 @@
 package org.example.lesson_2
 
+const val FULL_PERCENT = 100
 fun main() {
     val startingAmount = 70000
-    val percent = 16.7
-    val period = 20.0
+    val interestRare = 16.7
+    val period = 20
+    var finishAmount = 1.0
 
-    val finishAmount = startingAmount*(Math.pow((1+percent/100), period))
+    val amountOfYear = (1 + interestRare / FULL_PERCENT)
+
+    for (i in 1..period) {
+        finishAmount *= amountOfYear
+        println(finishAmount)
+        println(i)
+    }
+
+    finishAmount *= startingAmount
+
     print(String.format("%.3f", finishAmount))
 }

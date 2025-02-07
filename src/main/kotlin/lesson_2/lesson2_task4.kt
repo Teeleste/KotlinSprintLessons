37c60ec5d939611be074c001d28f679b6d33f21e
Teeleste <gnu4y.nikita2014@gmail.com>
2025-02-07 08:25:40 +0300

    KS-2-4 Replacing magic numbers

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,21 @@
 package org.example.lesson_2
 
+const val FULL_PERCENT = 100
+
 fun main() {
 
     val crystalsWithoutBuff = 7
     val ironsWithoutBuff = 11
     val buff = 20
-    val bonusCrystals = ((crystalsWithoutBuff * buff) / 100)
+
+    val bonusCrystals = ((crystalsWithoutBuff * buff) / FULL_PERCENT)
+
     val wholeCrystals: Int = bonusCrystals.toInt()
-    val bonusIrons = ((ironsWithoutBuff * buff) / 100)
+
+    val bonusIrons = ((ironsWithoutBuff * buff) / FULL_PERCENT)
+
     val wholeIrons: Int = bonusIrons.toInt()
+
     println("Количество дополнительных кристаллов = " + wholeCrystals)
     println("Количество дополнительной железной руды = " + wholeIrons)
 

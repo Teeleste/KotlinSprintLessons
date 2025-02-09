714e65174c1fd82f624bb385d5f68ff9003422e0
Teeleste <gnu4y.nikita2014@gmail.com>
2025-02-09 09:57:03 +0300

    KS-4-5 add const and changing conditions

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -3,10 +3,12 @@ package org.example.lesson_4
 const val MINIMUM_STAFF = 55
 const val MAXIMUM_STAFF = 70
 const val MINIMUM_PROVISIONS = 50
+const val NO_DAMAGE_DONE = true
+const val FAVORABLE_CONDITIONS_FOR_PLANE = true
 
 fun main() {
 
-    println("У корабля есть повреждения? ")
+    println("У корабля нет повреждений? ")
     val damage = readln().toBoolean()
 
     println("Текущий состав экипажа: ")
@@ -20,7 +22,7 @@ fun main() {
 
 
     print("Корабль может отправиться в плавание? : " +
-            "${((damage == false) && (crewComplement >= MINIMUM_STAFF && crewComplement <= MAXIMUM_STAFF ) && (numberBoxProvisions > 50))
-                    || ((damage == true) && (crewComplement == MAXIMUM_STAFF) && (numberBoxProvisions >= MINIMUM_PROVISIONS) && (favorableCondition == true))}")
+            "${((damage == NO_DAMAGE_DONE) && (crewComplement >= MINIMUM_STAFF && crewComplement <= MAXIMUM_STAFF ) && (numberBoxProvisions > MINIMUM_PROVISIONS))
+                    || ((damage != NO_DAMAGE_DONE) && (crewComplement == MAXIMUM_STAFF) && (numberBoxProvisions >= MINIMUM_PROVISIONS) && (favorableCondition == FAVORABLE_CONDITIONS_FOR_PLANE))}")
 
 }
